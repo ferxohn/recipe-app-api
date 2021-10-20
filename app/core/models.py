fb45699c5c8e0a30b53b830805271cea2b01a588
Fernando Gomez Perera <160300129@ucaribe.edu.mx>
2021-10-20 17:21:38 -0500

    Added recipe image upload feature.

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ def recipe_image_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
-    return os.path.join('uploads/recipe', filename)
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
